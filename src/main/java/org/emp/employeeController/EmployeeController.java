b343c898827af8cc5d9c7301a6997a80cd21287b
kalharaweragoda <kalharaweragoda@gmail.com>
2025-04-14 11:26:54 +0530

    higernate and add | get All test apis

--- src/main/java/org/emp/employeeController/EmployeeController.java
+++ src/main/java/org/emp/employeeController/EmployeeController.java
@@ -1,23 +1,33 @@
 package org.emp.employeeController;
 
+import lombok.RequiredArgsConstructor;
 import org.emp.dto.Employee;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.emp.entity.EmployeeEntity;
+import org.emp.service.EmployeeService;
+import org.emp.service.impl.EmployeeServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/emp-controller")
+@RequiredArgsConstructor
 //http://localhost:8080/emp-controller/add-employee
 public class EmployeeController {
 
-    List<Employee> employeeList = new ArrayList<>();
+    @Autowired
+    final EmployeeService service;
+    //EmployeeService service = new EmployeeServiceImpl();
 
     @PostMapping("add-employee")
     public void addEmployee(@RequestBody Employee employee){
-        employeeList.add(employee);
+        service.addEmployee(employee);
+    }
+
+    @GetMapping("get-all")
+    public List<EmployeeEntity> getAll(){
+        return service.getAll();
     }
 }
